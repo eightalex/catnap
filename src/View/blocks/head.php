7beed3f5fc97a9c67eba679e262c0800aa382f5b
akuzmenko <eight.alex@gmail.com>
2018-03-17 19:19:21 +0200

    Add head and bottom blocks

---
+++ src/View/blocks/head.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+    <title><?= $title ?></title>
+
+    <link rel="stylesheet" href="/css/normalize.css">
+    <link rel="stylesheet" href="/css/header.css">
+    <link rel="stylesheet" href="/css/footer.css">
+
+    <?= $styles ?>
+</head>
+<body class="page">
--- src/View/pages/main.php
+++ src/View/pages/main.php
@@ -1,28 +1,20 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
-    <title>Yousleep</title>
-
-    <link rel="stylesheet" href="/css/normalize.css">
-    <link rel="stylesheet" href="/css/footer.css">
-    <link rel="stylesheet" href="/css/gallery.css">
-    <link rel="stylesheet" href="/css/header.css">
-    <link rel="stylesheet" href="/css/page.css">
-    <link rel="stylesheet" href="/css/main-menu.css">
-    <link rel="stylesheet" href="/css/page-description.css">
-    <link rel="stylesheet" href="/css/page-title.css">
-</head>
-<body class="page">
-    <?php $this->renderBlock('header'); ?>
-    <?php $this->renderBlock('main-menu'); ?>
-    <?php $this->renderBlock('page-title', ['title' => 'Постельное белье']); ?>
-    <?php $this->renderBlock('page-description', ['text' =>
-        'Здесь, в магазине Usleep, белье — одно из наших любимых волокон. Как и просто супер
-        шить, льняная ткань имеет прекрасное земное ощущение, начиная с хрустящей корочки, но со временем смягчается с
-        мытьем и износом.']); ?>
-    <?php $this->renderBlock('gallery'); ?>
-    <?php $this->renderBlock('footer'); ?>
-</body>
-</html>
+<?php $this->renderBlock('head', [
+    'title' => 'Yousleep',
+    'styles' => '
+        <link rel="stylesheet" href="/css/gallery.css">
+        <link rel="stylesheet" href="/css/page.css">
+        <link rel="stylesheet" href="/css/main-menu.css">
+        <link rel="stylesheet" href="/css/page-description.css">
+        <link rel="stylesheet" href="/css/page-title.css">
+    '
+]); ?>
+<?php $this->renderBlock('header'); ?>
+<?php $this->renderBlock('main-menu'); ?>
+<?php $this->renderBlock('page-title', ['title' => 'Постельное белье']); ?>
+<?php $this->renderBlock('page-description', ['text' =>
+    'Здесь, в магазине Usleep, белье — одно из наших любимых волокон. Как и просто супер
+    шить, льняная ткань имеет прекрасное земное ощущение, начиная с хрустящей корочки, но со временем смягчается с
+    мытьем и износом.']); ?>
+<?php $this->renderBlock('gallery'); ?>
+<?php $this->renderBlock('footer'); ?>
+<?php $this->renderBlock('bottom'); ?>
