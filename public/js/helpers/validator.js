82a1b645ec12de9c347a3c5bdba7ff0a4d3c8401
akuzmenko <eight.alex@gmail.com>
2018-07-21 20:08:01 +0300

    Email validation

--- public/js/helpers/validator.js
+++ public/js/helpers/validator.js
@@ -6,6 +6,11 @@ var validator = (function() {
             }
 
             return integer % 1 === 0;
+        },
+
+        checkEmail: function(email) {
+            var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            return re.test(String(email).toLowerCase());
         }
     };
 })();
